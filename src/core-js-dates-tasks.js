8f7daed7946d253f7ca8d5d4f07412f78f190448
Katerina <comina.ru@gmail.com>
2026-01-04 18:56:29 +0200

    feat: add 3 passing

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -138,8 +138,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dat = new Date(date);
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+  return dat >= start && dat <= end;
 }
 
 /**
@@ -153,8 +156,18 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dat = new Date(date);
+  const month = dat.getUTCMonth() + 1;
+  const day = dat.getUTCDate();
+  const year = dat.getUTCFullYear();
+  const minutes = dat.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = dat.getUTCSeconds().toString().padStart(2, '0');
+  let hours = dat.getUTCHours();
+  const amPm = hours >= 12 ? 'PM' : 'AM';
+  hours %= 12;
+  hours = hours || 12;
+  return `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${amPm}`;
 }
 
 /**
@@ -169,8 +182,20 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const lastDay = new Date(year, month, 0);
+  const daysInMonth = lastDay.getDate();
+  let weekendCount = 0;
+
+  for (let day = 1; day <= daysInMonth; day += 1) {
+    const date = new Date(year, month - 1, day);
+    const dayOfWeek = date.getDay();
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      weekendCount += 1;
+    }
+  }
+
+  return weekendCount;
 }
 
 /**
